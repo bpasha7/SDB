d3c6b4ed83d9d297563bf72ff4c69922434f330d
Paul Bezruk <Paul Bezruk>
2017-11-27 22:36:15 +0700

    Add project files.

---
+++ mdb_win/DataBase.h
@@ -0,0 +1,70 @@
+#pragma region Includes
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <thread>
+#pragma endregion
+
+using namespace std;
+
+
+
+#pragma region Defines
+
+#pragma endregion
+#pragma warning( push )
+#pragma warning( disable : 4996)
+class Command
+{
+public:
+	char* commandWord;
+	char* commandString;
+	int countWords;
+	char** Words;
+	void ParseWords()
+	{
+		char* word = new char[255];
+		Words = new char * [countWords];
+		int pos = 0;
+		int count = 0;
+		for (size_t i = 0; i < strlen(commandString); i++)
+		{
+			if (commandString[i] == ' ')
+			{
+				word[pos] = '\0';
+				Words[count] = new char[pos];
+				strcpy(Words[count], word);
+				count++;
+				pos = 0;
+			}
+			else
+			{
+				word[pos++] = commandString[i];
+			}
+		}
+		word[pos] = '\0';
+		Words[count] = new char[pos];
+		strcpy(Words[count], word);
+	}
+};
+#pragma once
+class DataBase
+{
+private:
+	char* _version;
+	char* _errorMessage;
+	ofstream _dataBase;
+	int DoCommand(char * command);
+	Command * ToLowevCase(char * command);
+public:
+	DataBase();
+	int ReadCommands();
+	int Create(char* name);
+	int Start();
+	~DataBase();
+};
+
+
+
+#pragma warning( pop ) 
